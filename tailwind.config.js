9e8c98b9ee7e147f058cb6239dae146a3d80da64
Meghla15 <meherunnesa1505@gmail.com>
2024-01-23 21:57:56 +0600

    last changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,6 @@ module.exports = {
 
 
 
-  <div class="absolute flex justify-between lg:justify-normal transform -translate-y-1/2 lg:left-7  lg:top-[90%] lg:gap-3 left-5 right-5 top-1/2"></div>
-
+ 
 
   
